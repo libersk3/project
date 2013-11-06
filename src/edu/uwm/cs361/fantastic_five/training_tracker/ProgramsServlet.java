78728108fea6c08f63f8a4a9c49ef03e995adbfe
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-06 11:36:37 -0600

    Add page for viewing a list of all programs

---
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/ProgramsServlet.java
@@ -0,0 +1,38 @@
+package edu.uwm.cs361.fantastic_five.training_tracker;
+
+import java.io.IOException;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.*;
+
+import java.util.List;
+
+import edu.uwm.cs361.fantastic_five.training_tracker.entities.Program;
+
+@SuppressWarnings("serial")
+public class ProgramsServlet extends HttpServlet {
+	@SuppressWarnings("unchecked")
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PersistenceManager pm = getPersistenceManager();
+		
+		resp.setContentType("text/html");
+
+		resp.getWriter().println("<ul>");
+		for (Program program : (List<Program>) pm.newQuery(Program.class).execute()) {
+			resp.getWriter().println(program.getName());
+			resp.getWriter().println("<br/>");
+			resp.getWriter().println(program.getInstructor());
+			resp.getWriter().println("<br/>");
+			resp.getWriter().println(program.getPrice());
+			resp.getWriter().println("<br/>");
+			resp.getWriter().println("<hr/>");
+		}
+		resp.getWriter().println("<a href='programs/new'>New Program</a>");
+	}
+
+	private PersistenceManager getPersistenceManager()
+	{
+		return JDOHelper.getPersistenceManagerFactory("transactions-optional").getPersistenceManager();
+	}
+}
