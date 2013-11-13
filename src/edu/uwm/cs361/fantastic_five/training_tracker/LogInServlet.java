eba210d25c1de19d3d670cec4d8a4a8c66883818
cassie1992 <dowling@uwm.edu>
2013-11-12 18:29:42 -0600

    Update LogInServlet.java

--- src/edu/uwm/cs361/fantastic_five/training_tracker/LogInServlet.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/LogInServlet.java
@@ -1,13 +1,13 @@
 package edu.uwm.cs361.fantastic_five.training_tracker;
 import java.io.IOException;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Query;
 
@@ -19,6 +19,9 @@ public class LogInServlet extends HttpServlet
 	{
 		resp.setContentType("text/html");
 		resp.getWriter().println("<h1> Log In </h1>");
+		if (req.getParameter("error") != null)
+			resp.getWriter().println("<h3>**Username or password is incorrect**</h3>");
+		
 		resp.getWriter().println("<form action= '/login' method='POST'>"); 
 		resp.getWriter().println("<br><br>Username: <input type='text' name='username'/>");
 		resp.getWriter().println("<br>Password: <input type='password' name='password'/>");
@@ -30,32 +33,19 @@ public class LogInServlet extends HttpServlet
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-		Entity user = new Entity("User");
-		user.setProperty("username", "admin");
-		user.setProperty("password", "password");
-		user.setProperty("access", "Admin");
-		ds.put(user);
 		Query q = new Query("User").setFilter(
 				Query.CompositeFilterOperator.and(
-						new Query.FilterPredicate("username",Query.FilterOperator.EQUAL,req.getParameter("username")),
-						new Query.FilterPredicate("password",Query.FilterOperator.EQUAL,req.getParameter("password"))
-						)
-					);
-		
+					new Query.FilterPredicate("username",Query.FilterOperator.EQUAL,req.getParameter("username")),
+					new Query.FilterPredicate("password",Query.FilterOperator.EQUAL,req.getParameter("password"))
+					)
+				);
 		if (ds.prepare(q).countEntities(FetchOptions.Builder.withDefaults()) > 0)
 		{
-			resp.sendRedirect("/homepage");
+			Cookie c = new Cookie("username",req.getParameter("username"));
+			resp.addCookie(c);
+			resp.sendRedirect("/user");
 		}
 		else
-		{
-			resp.setContentType("text/html");
-			resp.getWriter().println("<h1> Log In</h1>");
-			resp.getWriter().println("<h3>**Username or password is incorrect**</h3>");
-			resp.getWriter().println("<form action= '/login' method='POST'>");
-			resp.getWriter().println("<br>Username: <input type='text' name='username'/>");
-			resp.getWriter().println("<br>Password: <input type='password' name='password'/>");
-			resp.getWriter().println("<br><input type='submit' value='Log In'/>");
-			resp.getWriter().println("</form>");
-		}
+			resp.sendRedirect("/login?error=1");
 	}
 }
