badc31b2d83e531e471afb5b94d484fb319d5aa4
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-12 18:03:21 -0600

    Fix unowned attribute in program entitity

--- src/edu/uwm/cs361/fantastic_five/training_tracker/entities/Program.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/entities/Program.java
@@ -1,12 +1,14 @@
 package edu.uwm.cs361.fantastic_five.training_tracker.entities;
 
 import java.util.Set;
+
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 import com.google.appengine.api.datastore.Key;
+import com.google.appengine.datanucleus.annotations.Unowned;
 
 @PersistenceCapable
 public class Program {
