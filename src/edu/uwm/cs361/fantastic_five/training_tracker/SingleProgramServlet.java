d2cd228a15e534a7e00ebd312dd660472d055863
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-14 12:56:00 -0600

    Add JSP for SingleProgramServlet; fixes #17

--- src/edu/uwm/cs361/fantastic_five/training_tracker/SingleProgramServlet.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/SingleProgramServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import java.util.Set;
@@ -13,25 +15,19 @@ import edu.uwm.cs361.fantastic_five.training_tracker.entities.Student;
 
 @SuppressWarnings("serial")
 public class SingleProgramServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		PersistenceManager pm = getPersistenceManager();
 
 		String id = req.getParameter("id");
 		long idLong = Long.parseLong(id);
-		Program program = pm.getObjectById(Program.class,idLong);
+		Program program = pm.getObjectById(Program.class, idLong);
 
-		if (program != null) {
-			resp.setContentType("text/html");
-			resp.getWriter().println("<h1> Enrolled Students in " + program.getName() + ": </h1>");
-			resp.getWriter().println("<ul>");
-			Set<Student> students = program.listStudents();
-			for (Student student : students)
-				resp.getWriter().println("<br>" +student.getFirstName() + " " + student.getLastName());
-			resp.getWriter().println("</ul>");
-			resp.getWriter().println("<a href='/programs/enroll?id="+program.getKey().getId()+"'>Enroll Student</a>");
-		}
-		resp.getWriter().println("<br><br><a href='/homepage'>Home</a>");
-		resp.getWriter().println("<a href='/login' method='GET'>Log Out</a>");
+		req.setAttribute("program", program);
+		if (program != null) req.setAttribute("students", program.listStudents());
+
+		String nextJSP = "/WEB-INF/pages/program.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+		dispatcher.forward(req, resp);
 	}
 
 	private PersistenceManager getPersistenceManager()
---
+++ war/WEB-INF/pages/program.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<t:mainLayout title="${program.name}">
+	<c:if test="${not empty program}">
+		<h1>Enrolled Students in ${program.name}:</h1>
+		<ul>
+			<c:forEach items="${students}" var="student">
+				<li>${student.fullName}</li>
+			</c:forEach>
+		</ul>
+		<a href='/programs/enroll?id=${program.key.id}'>Enroll Student</a>
+	</c:if>
+</t:mainLayout>
