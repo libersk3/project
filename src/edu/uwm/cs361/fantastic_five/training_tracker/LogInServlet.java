8468e3d793595093dcaec4b05e229c6e9ab3aa32
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-14 14:45:51 -0600

    Add JSP for LoginServlet; fixes #18

--- src/edu/uwm/cs361/fantastic_five/training_tracker/LogInServlet.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/LogInServlet.java
@@ -1,6 +1,8 @@
 package edu.uwm.cs361.fantastic_five.training_tracker;
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,22 +17,15 @@ import com.google.appengine.api.datastore.Query;
 public class LogInServlet extends HttpServlet
 {
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
-		resp.setContentType("text/html");
-		resp.getWriter().println("<h1> Log In </h1>");
-		if (req.getParameter("error") != null)
-			resp.getWriter().println("<h3>**Username or password is incorrect**</h3>");
-
-		resp.getWriter().println("<form action= '/login' method='POST'>");
-		resp.getWriter().println("<br><br>Username: <input type='text' name='username'/>");
-		resp.getWriter().println("<br>Password: <input type='password' name='password'/>");
-		resp.getWriter().println("<br><input type='submit' value='Log In'/>");
-		resp.getWriter().println("</form>");
+		String nextJSP = "/WEB-INF/pages/login.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+		dispatcher.forward(req, resp);
 	}
 
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		Query q = new Query("User").setFilter(
@@ -39,13 +34,16 @@ public class LogInServlet extends HttpServlet
 					new Query.FilterPredicate("password",Query.FilterOperator.EQUAL,req.getParameter("password"))
 					)
 				);
-		if (ds.prepare(q).countEntities(FetchOptions.Builder.withDefaults()) > 0)
-		{
+		if (ds.prepare(q).countEntities(FetchOptions.Builder.withDefaults()) > 0) {
 			Cookie c = new Cookie("username",req.getParameter("username"));
 			resp.addCookie(c);
 			resp.sendRedirect("/user");
+		} else {
+			req.setAttribute("error", true);
+
+			String nextJSP = "/WEB-INF/pages/login.jsp";
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+			dispatcher.forward(req, resp);
 		}
-		else
-			resp.sendRedirect("/login?error=1");
 	}
 }
---
+++ war/WEB-INF/pages/login.jsp
@@ -0,0 +1,23 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+
+<t:mainLayout title="Log In">
+	<h1>Log In</h1>
+	<c:if test="${not empty error}">
+		<div class="error-message">**Username or password is incorrect**</div>
+	</c:if>
+
+	<form class='well' action='' method='POST'>
+		<div class="field">
+			<label for='username'>Username:</label>
+			<input type='text' id='username' name='username' />
+		</div>
+		<div class="field">
+			<label for='password'>Password:</label>
+			<input type='password' id='password' name='password' />
+		</div>
+		<input type='submit' value='Log In'/>
+	</form>
+</t:mainLayout>
