f209d3bba91ec03f8c73eb24dc53a86c708273f3
Libersk3 <libersk3@uwm.edu>
2013-11-20 11:26:39 -0600

    fixed eclipse, edited files when did so, here are the changed ones that work now

--- src/edu/uwm/cs361/fantastic_five/training_tracker/entities/Program.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/entities/Program.java
@@ -6,8 +6,8 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
-
 import com.google.appengine.api.datastore.Key;
+
 import com.google.appengine.datanucleus.annotations.Unowned;
 
 @PersistenceCapable
