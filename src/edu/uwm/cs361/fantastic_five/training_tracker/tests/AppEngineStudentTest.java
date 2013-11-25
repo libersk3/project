46e697309dd7abf62bc399f40c42661ca5e2a918
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-25 11:23:10 -0600

    Add getKey test for the student entity

---
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/tests/AppEngineStudentTest.java
@@ -0,0 +1,33 @@
+package edu.uwm.cs361.fantastic_five.training_tracker.tests;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import javax.jdo.PersistenceManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.uwm.cs361.fantastic_five.training_tracker.app.entities.Student;
+
+public class AppEngineStudentTest extends AppEngineTest {
+	Student student;
+
+	@Before
+	public void setUpTest() {
+		this.student = new Student("George", "Washington", "gw@gmail.com");
+	}
+
+	@Test
+	public void testKey() {
+		Student student2 = new Student("George", "Washington", "gw@gmail.com");
+
+		PersistenceManager pm = getPersistenceManager();
+
+		pm.makePersistent(student);
+		pm.makePersistent(student2);
+
+		assertThat(student.getKey(), is(not(student2.getKey())));
+	}
+}
