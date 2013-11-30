ea2566134ef123af988fbd79b07748de0755b432
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-30 13:18:52 -0600

    Handle validation errors while creating programs

--- src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/ProgramCreator.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/ProgramCreator.java
@@ -1,5 +1,7 @@
 package edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases;
 
+import java.util.ArrayList;
+
 import javax.jdo.PersistenceManager;
 
 import edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases.requests.CreateProgramRequest;
@@ -10,14 +12,45 @@ import edu.uwm.cs361.fantastic_five.training_tracker.services.PersistenceService
 public class ProgramCreator {
 	public CreateProgramResponse createProgram(CreateProgramRequest req) {
 		PersistenceManager pm = getPersistenceManager();
+		CreateProgramResponse resp = new CreateProgramResponse();
+
+		if (req.name == null || req.name.isEmpty()) {
+			resp.errors.put("name", new ArrayList<String>());
+			resp.errors.get("name").add("Name must not be blank.");
+		}
+
+		if (req.instructor == null || req.instructor.isEmpty()) {
+			resp.errors.put("instructor", new ArrayList<String>());
+			resp.errors.get("instructor").add("Instructor must not be blank.");
+		}
+
+		ArrayList<String> priceErrors = new ArrayList<String>();
+		double price = 0;
+		if (req.price == null || req.price.isEmpty()) {
+			priceErrors.add("Price must not be blank.");
+		} else {
+			try {
+				price = Double.parseDouble(req.price);
+			} catch (NumberFormatException ex) {
+				priceErrors.add("Price is invalid.");
+			}
+		}
+		if (!priceErrors.isEmpty()) resp.errors.put("price", priceErrors);
+
+		if (!resp.errors.isEmpty()) {
+			resp.success = false;
+			return resp;
+		} else {
+			resp.success = true;
+		}
 
 		try {
-			pm.makePersistent(new Program(req.name, req.instructor, Double.parseDouble(req.price)));
+			pm.makePersistent(new Program(req.name, req.instructor, price));
 		} finally {
 			pm.close();
 		}
 
-		return new CreateProgramResponse();
+		return resp;
 	}
 
 	private PersistenceManager getPersistenceManager()
--- src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/responses/CreateProgramResponse.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/responses/CreateProgramResponse.java
@@ -1,4 +1,10 @@
 package edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases.responses;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CreateProgramResponse {
+	public boolean success;
+	public Map<String, List<String>> errors = new HashMap<String, List<String>>();
 }
