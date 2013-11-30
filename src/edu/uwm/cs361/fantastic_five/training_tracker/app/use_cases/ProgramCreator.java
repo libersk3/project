08f90db17e9ea8aa87333472e30046f003cac07d
Andrew Meyer <andrewm.bpi@gmail.com>
2013-11-30 14:39:58 -0600

    Refactor program validation

--- src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/ProgramCreator.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/ProgramCreator.java
@@ -1,51 +1,28 @@
 package edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases;
 
-import java.util.ArrayList;
-
 import javax.jdo.PersistenceManager;
 
 import edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases.requests.CreateProgramRequest;
 import edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases.responses.CreateProgramResponse;
 import edu.uwm.cs361.fantastic_five.training_tracker.app.entities.Program;
 import edu.uwm.cs361.fantastic_five.training_tracker.services.PersistenceService;
+import edu.uwm.cs361.fantastic_five.training_tracker.services.ProgramValidator;
 
 public class ProgramCreator {
 	public CreateProgramResponse createProgram(CreateProgramRequest req) {
 		PersistenceManager pm = getPersistenceManager();
 		CreateProgramResponse resp = new CreateProgramResponse();
 
-		if (req.name == null || req.name.isEmpty()) {
-			resp.errors.put("name", new ArrayList<String>());
-			resp.errors.get("name").add("Name must not be blank.");
-		}
-
-		if (req.instructor == null || req.instructor.isEmpty()) {
-			resp.errors.put("instructor", new ArrayList<String>());
-			resp.errors.get("instructor").add("Instructor must not be blank.");
-		}
-
-		ArrayList<String> priceErrors = new ArrayList<String>();
-		double price = 0;
-		if (req.price == null || req.price.isEmpty()) {
-			priceErrors.add("Price must not be blank.");
-		} else {
-			try {
-				price = Double.parseDouble(req.price);
-			} catch (NumberFormatException ex) {
-				priceErrors.add("Price is invalid.");
-			}
-		}
-		if (!priceErrors.isEmpty()) resp.errors.put("price", priceErrors);
+		resp.errors = new ProgramValidator().validate(req.name, req.instructor, req.price);
 
 		if (!resp.errors.isEmpty()) {
 			resp.success = false;
 			return resp;
-		} else {
-			resp.success = true;
 		}
 
 		try {
-			pm.makePersistent(new Program(req.name, req.instructor, price));
+			pm.makePersistent(new Program(req.name, req.instructor, Double.parseDouble(req.price)));
+			resp.success = true;
 		} finally {
 			pm.close();
 		}
