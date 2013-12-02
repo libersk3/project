07354f90cefb71377dc9bc2b048b0f71db88da19
Andrew Meyer <andrewm.bpi@gmail.com>
2013-12-01 22:43:21 -0600

    Remove unnecessary warning suppression

--- src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/Authenticator.java
+++ src/edu/uwm/cs361/fantastic_five/training_tracker/app/use_cases/Authenticator.java
@@ -7,13 +7,12 @@ import edu.uwm.cs361.fantastic_five.training_tracker.app.use_cases.responses.Log
 import edu.uwm.cs361.fantastic_five.training_tracker.services.PersistenceService;
 
 public abstract class Authenticator {
-	
-	
-	@SuppressWarnings("unchecked")
+
+
 	public abstract LogInResponse authenticate(LogInRequest req);
-	
+
 	protected PersistenceManager getPersistenceManager() {
 		return PersistenceService.getPersistenceManager();
 	}
-	
+
 } //end interface
